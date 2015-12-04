02125e8bf5245886b2080271df4c4287401522d9
liyy7 <aleeyyu+01@gmail.com>
2015-12-04 18:55:18 +0900

    10things: Using close to broadcast

---
+++ 10things/9b_using_close_to_broadcast.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Work struct {
+	number int
+}
+
+func (w Work) String() string {
+	return fmt.Sprintf("work_%d", w.number)
+}
+func (w Work) Refuse() {}
+func (w Work) Do()     {}
+
+func worker(i int, ch chan Work, quit chan struct{}) {
+	var quitting bool
+	for {
+		time.Sleep(100 * time.Millisecond)
+		select {
+		case w := <-ch:
+			if quitting {
+				w.Refuse()
+				fmt.Println("worker", i, "refused", w)
+				return
+			}
+			w.Do()
+			fmt.Println("worker", i, "processed", w)
+		case <-quit:
+			fmt.Println("worker", i, "quitting")
+			quitting = true
+		}
+	}
+}
+
+func main() {
+	ch, quit := make(chan Work), make(chan struct{})
+	go func() {
+		for i := 0; ; i++ {
+			ch <- Work{number: i}
+		}
+	}()
+	for i := 0; i < 4; i++ {
+		go worker(i, ch, quit)
+	}
+	time.Sleep(5 * time.Second)
+	close(quit)
+	time.Sleep(2 * time.Second)
+}
